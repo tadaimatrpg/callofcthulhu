e05468d6f3eed9c141e3b2def64ca530da942a4f
ただいま <54084706+tadaimatrpg@users.noreply.github.com>
2020-06-10 17:59:12 +0900

    Save the definition of each language

---
+++ module/config.js
@@ -0,0 +1,25 @@
+export const CallofCthulhu = {}
+
+CallofCthulhu.abilities = {
+  "HP": "CallofCthulhu.AbilityHP",
+  "MP": "CallofCthulhu.AbilityMP",
+  "Luck": "CallofCthulhu.AbilityLuck",
+  "Sanity": "CallofCthulhu.AbilitySanity",
+  "Indef": "CallofCthulhu.AbilityIndef",
+  "armor": "CallofCthulhu.Abilityarmor",
+  "Flesh_ward": "CallofCthulhu.AbilityFleshward",
+  "DEX": "CallofCthulhu.AbilityDEX",
+  "Move": "CallofCthulhu.AbilityMove",
+  "Character_sheet": "CallofCthulhu.AbilityCharacter sheet",
+  "Item": "CallofCthulhu.AbilityItem",
+  "name": "CallofCthulhu.AbilityItem"
+}
+
+CallofCthulhu.aitms = {
+    "malfuncti": "CallofCthulhu.aitmsmalfuncti",
+    "distance": "CallofCthulhu.aitmsdistance",
+    "RoF": "CallofCthulhu.aitmsRoF",
+    "capacity": "CallofCthulhu.aitmscapacity",
+    "Damage": "CallofCthulhu.aitmsFleshward",
+    "Explanation": "CallofCthulhu.aitmsExplanation"
+  }
