5f5c002c611b6b18435612e3a32945ddb37d134b
kaitorogatukaenai <54084706+kaitorogatukaenai@users.noreply.github.com>
2020-07-05 15:50:28 +0900

    Upgrade By Bug Fixes

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -29,12 +29,15 @@ export class CallofCthulhuActorSheet extends ActorSheet {
     return  data;
   }
 
-/** @override */
-activateListeners(html) {
+ /** @override */
+ activateListeners(html) {
   super.activateListeners(html);
 
   // Everything below here is only needed if the sheet is editable
   if (!this.options.editable) return;
+
+  // Add Inventory Item
+  html.find('.item-create').click(this._onItemCreate.bind(this));
 
   // Update Inventory Item
   html.find('.item-edit').click(ev => {
@@ -49,11 +52,34 @@ activateListeners(html) {
     this.actor.deleteOwnedItem(li.data("itemId"));
     li.slideUp(200, () => this.render(false));
   });
-
-  // Add or Remove Attribute
-  html.find(".attributes").on("click", ".attribute-control", this._onClickAttributeControl.bind(this));
 }
 
+ /**
+   * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  _onItemCreate(event) {
+    event.preventDefault();
+    const header = event.currentTarget;
+    // Get the type of item to create.
+    const type = header.dataset.type;
+    // Grab any data associated with this control.
+    const data = duplicate(header.dataset);
+    // Initialize a default name.
+    const name = `New ${type.capitalize()}`;
+    // Prepare the item object.
+    const itemData = {
+      name: name,
+      type: type,
+      data: data
+    };
+    // Remove the type from the dataset since it's in the itemData.type prop.
+    delete itemData.data["type"];
+
+    // Finally, create the item!
+    return this.actor.createOwnedItem(itemData);
+  }
 /* -------------------------------------------- */
 
 /** @override */
@@ -65,36 +91,37 @@ setPosition(options={}) {
   return position;
 }
 
-/**
-   * Listen for click events on an attribute control to modify the composition of attributes in the sheet
-   * @param {MouseEvent} event    The originating left click event
-   * @private
-   */
-  async _onClickAttributeControl(event) {
-    event.preventDefault();
-    const a = event.currentTarget;
-    const action = a.dataset.action;
-    const attrs = this.object.data.data.attributes;
-    const form = this.form;
+// /**
+//    * Listen for click events on an attribute control to modify the composition of attributes in the sheet
+//    * @param {MouseEvent} event    The originating left click event
+//    * @private
+//    */
+//   async _onClickAttributeControl(event) {
+//     event.preventDefault();
+//     const a = event.currentTarget;
+//     const action = a.dataset.action;
+//     const attrs = this.object.data.data.attributes;
+//     const form = this.form;
 
-    // Add new attribute
-    if ( action === "create" ) {
-      const nk = Object.keys(attrs).length + 1;
-      let newKey = document.createElement("div");
-      newKey.innerHTML = `<input type="text" name="data.attributes.attr${nk}.key"/>`;
-      newKey = newKey.children[0];
-      form.appendChild(newKey);
-      await this._onSubmit(event);
-    }
+//     // Add new attribute
+//     if ( action === "create" ) {
+//       const nk = Object.keys(attrs).length + 1;
+//       let newKey = document.createElement("div");
+//       newKey.innerHTML = `<input type="text" name="data.attributes.attr${nk}.key"/>`; 
+//       newKey = newKey.children[0];
+//       form.appendChild(newKey);
+//       await this._onSubmit(event);
+//     }
 
-    // Remove existing attribute
-    else if ( action === "delete" ) {
-      const li = a.closest(".attribute");
-      li.parentElement.removeChild(li);
-      await this._onSubmit(event);
-    }
-  }
+//     // Remove existing attribute
+//     else if ( action === "delete" ) {
+//       const li = a.closest(".attribute");
+//       li.parentElement.removeChild(li);
+//       await this._onSubmit(event);
+//     }
+//   }
     /* -------------------------------------------- */
+
 }
  
 
