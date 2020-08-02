b4d62a5ed7b63e436ba3af188fad38163ed00d14
kaitorogatukaenai <54084706+kaitorogatukaenai@users.noreply.github.com>
2020-08-02 10:44:25 +0900

    update

--- module/dicebot.js
+++ module/dicebot.js
@@ -1,8 +1,9 @@
 /**
  *CallofCthulhu(7thEd.) Define your own commands
  *Here, we receive the chat Messages and determine each command.
- *[/CC or /CC(x)]Define a command to judge a normal dice (bonus penalty).
+ *[/CC]Define a command to judge a normal dice.
  *[/CBR] Command to define the decision on combination rolls.
+ *[/BP] Determine a bonus or penalty die.(not yet implemented)
 */
 Hooks.on("chatMessage", (html,content) => {
 //Read the command
@@ -20,11 +21,8 @@ Hooks.on("chatMessage", (html,content) => {
     let res = "<h1>Error</h1>";
     //Normal Dice
     let r = new Roll("1d100");
-     //r2 and r3 are used to determine bonus penalties 
-    let r2 = new Roll("1d10");
-    let r3 = new Roll("1d10");
-
-
+     //r2 are used to determine bonus penalties 
+    // let r2 = new Roll("1d10");
    //Perform processing for each command.
    if(command === "/CC" || command === "/cc"){
     //This time, extract only the numbers.
@@ -55,6 +53,8 @@ Hooks.on("chatMessage", (html,content) => {
         return false;
     }
 
+
+    //Combination rolls
     if(command === "/CBR" || command === "/cbr"){
         //Extracting numbers from combination rolls
         rgx = /(?:[0-9]+)/g;   
@@ -63,7 +63,7 @@ Hooks.on("chatMessage", (html,content) => {
         rgx = /(?:,[0-9]+)/g;   
         commands = content.match(rgx);
         n = commands[0].replace(/[^0-9]/g, '');
-
+        
         //Determination based on rolls and readings
         r.roll();
         //Put the result of the die into s as a number rather than an object.
@@ -98,4 +98,45 @@ Hooks.on("chatMessage", (html,content) => {
         //return to avoid errors in the command.
             return false;
         }
+
+    // //Bonus, if the penalty die is correct.
+    // if(command === "/BP" || command === "/bp"){
+    //     //Extracting numbers from combination rolls
+    //     rgx = /(?:[^-0-9]+)/g;   
+    //     commands = content.match(rgx);
+    //     n = commands[0];
+    //     rgx = /(?:,[0-9]+)/g;   
+    //     commands = content.match(rgx);
+    //     m = commands[0].replace(/[^0-9]/g, '');
+
+    //     //Determination based on rolls and readings
+    //     if(n >= 0){
+    //     r.roll(n);
+    //     r.keepLo​​west(1);
+    //     }else{
+    //     r.roll(n);
+    //     r.keepHighest(1);
+    //     }
+    //     //Put the result of the die into s as a number rather than an object.
+    //     s = r.result;
+    //     //Determine the first number.
+    //     if(s <= 1) res = game.i18n.localize("CallofCthulhu.dicebotCritical");
+    //     else if(s >= 100) res= game.i18n.localize("CallofCthulhu.dicebotFumble");
+    //     else if(s <= (m/5)) res= game.i18n.localize("CallofCthulhu.dicebotExtreme");
+    //     else if(s <= (m/2)) res= game.i18n.localize("CallofCthulhu.dicebotHard");
+    //     else if(s <= m) res= game.i18n.localize("CallofCthulhu.dicebotRegular");
+    //     else if(s >= 96){if(m < 50) res= game.i18n.localize("CallofCthulhu.dicebotFumble"); 
+    //     else res= game.i18n.localize("CallofCthulhu.dicebotFail");}
+    //     else res= game.i18n.localize("CallofCthulhu.dicebotFail");
+    //     //Record the first results.
+    //     res += (game.i18n.localize("CallofCthulhu.dicebotSkill") + m);
+
+    //     r.toMessage(
+    //         {speaker: ChatMessage.getSpeaker(),
+    //         flavor: res,
+    //         });
+    //     //return to avoid errors in the command.
+    //         return false;
+    //     }
+ 
 })
